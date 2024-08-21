0ec5a9a9f9ec14581d129224b90a563f7a8c53a7
Ziad Ahmed <ziad.a200343@gmail.com>
2024-08-21 09:16:09 +0300

    updated version

--- Script.js
+++ Script.js
@@ -1,1 +1,7 @@
-let arr = ['Enzl ya mtdala3!', 'El gayar l yakhtashy', '#andoko shamboo??'];
+let arr = ['Kindness is one thing you can’t give away. It always comes back.', 'Friends are the family we choose', 'My purpose: to lift your spirit and to motivate you.',
+   'Try to be a rainbow in someone else’s cloud.' ];
+function random_message(messages){
+    let random = Math.floor(Math.random() * arr.length); 
+    return messages[random];
+}
+console.log(random_message(arr));
