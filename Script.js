01b86de6a88e72c44c53b744f320e3815afdbedc
Ziad Ahmed <ziad.a200343@gmail.com>
2024-08-21 10:55:50 +0300

    added gui for the js

--- Script.js
+++ Script.js
@@ -1,7 +1,27 @@
-let arr = ['Kindness is one thing you can’t give away. It always comes back.', 'Friends are the family we choose', 'My purpose: to lift your spirit and to motivate you.',
-   'Try to be a rainbow in someone else’s cloud.' ];
-function random_message(messages){
-    let random = Math.floor(Math.random() * arr.length); 
+let arr = [
+    'Kindness is one thing you can not give away. It always comes back.', 
+    'Friends are the family we choose', 
+    'My purpose: to lift your spirit and to motivate you.',
+    'Try to be a rainbow in someone else cloud.'
+];
+
+function random_message(messages) {
+    let random = Math.floor(Math.random() * messages.length); 
     return messages[random];
 }
-console.log(random_message(arr));
+
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById("button");
+    const printArea = document.getElementById("print-area");
+
+    button.addEventListener('click', function(){
+        // Get a random message
+        const message = random_message(arr);
+
+        // Update the content of the print area
+        printArea.innerHTML = `<h1>${message}</h1>`;
+
+        // Print the content of the print area
+        window.print();
+    });
+});
